02d2bb8617435d05c1a496fa128a14b21be7a290
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-28 02:43:54 +0600

    absar

--- src/Components/HomeComponents/HomePage/HomePage.jsx
+++ src/Components/HomeComponents/HomePage/HomePage.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import logo from '../../images/et.png';
+const homeData=[
+    {
+       name: 'Food', 
+       image: logo,
+       price: 160,
+       
+    },
+    {
+        name: 'Food', 
+        image: logo,
+        price: 160,
+    },
 
+
+]
 const HomePage = () => {
+
+const slides = [
+        { title: 'First item', description: 'Lorem ipsum', logo: logo},
+        { title: 'Second item', description: 'Lorem ipsum', logo: logo}
+      ];
     return (
-        <div>
-            <h4>Home Page</h4>
-        </div>
+        <Slider>
+            {slides.map((slide, index) => <div key={index}>
+                <h2>{slide.title}</h2>
+                <div>{slide.description}</div>
+                <img src={slide.logo} alt="" />
+            </div>)}
+       </Slider>
     );
 };
 
