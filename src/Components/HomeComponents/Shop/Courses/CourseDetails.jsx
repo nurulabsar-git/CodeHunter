ca98441e03ef998bbc73068244ba4849c2f19181
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-07-28 15:47:36 +0600

    Absar

--- src/Components/HomeComponents/Shop/Courses/CourseDetails.jsx
+++ src/Components/HomeComponents/Shop/Courses/CourseDetails.jsx
@@ -11,7 +11,7 @@ const CourseDetails = () => {
     return (
         <Container style={style}>
           <h4>This is course {courseId} details page</h4> 
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/pKO9UjSeLew" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/kiUGf_Z08RQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </Container>
     );
 };
